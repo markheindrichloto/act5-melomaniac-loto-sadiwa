edf17e1c0ec70146deb2c57932b5a7c5ece1439c
LotoMarkHeindrich <markheindrichloto@gmail.com>
2024-03-12 12:21:25 +0800

    High School LSS

---
+++ song1.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<head>
+    <title>Song 1</title>
+    
+<head>
+<body style = "text-align:center;">
+    <?php
+    echo "<h1>Pusong Bato</h1>";
+    echo "by Jovit Baldivino and Rene De La Rosa<br>";
+    echo "Lyrics and Guitar Chords<br>";
+    echo "     <br>";
+
+    echo"[Verse 1]<br>";
+    echo"D           G<br>";
+    echo"Nang ika`y ibigin ko<br>";
+    echo"    A              D<br>";
+    echo"Mundo ko`y biglang nagbago<br>";
+    echo"D               G<br>";
+    echo"Akala ko ika`y langit<br>";
+    echo"A               D<br>";
+    echo"Yun pala`y sakit ng ulo<br>";
+    echo"D               G<br>";
+    echo"Sabi mo noon sa `kin <br>";
+    echo"    A             D<br>";
+    echo"Kailan may di mag  babago<br>";
+    echo" D                 G<br>";
+    echo"Naniwala naman sa`yo<br>";
+    echo" A          D  A<br>";
+    echo"Ba`t ngayo`y iniwan mo<br>";
+
+
+    echo"[Chorus]<br>";
+
+    echo"D<br>";
+    echo"Di mo alam dahil sa`yo<br>";
+    echo"G<br>";
+    echo"Ako`y di makakain<br>";
+    echo"A<br>";
+    echo"Di rin makatulog <br>";
+    echo"D                    A<br>";
+    echo"Buhat ng iyong lokohin<br>";
+    echo"D<br>";
+    echo"Kung ako`y muling iibig <br>";
+    echo"G<br>";
+    echo"Sana`y di maging katulad mo<br>";
+    echo"A                        D  A<br>";
+    echo"Tulad mo nama`y pusong bato<br>";
+
+
+    echo"[Verse 2]<br>";
+
+    echo"    D              G<br>";
+    echo"Kahit san ka man ngayon<br>";
+    echo"    A         D<br>";
+    echo"Dinggin mo itong awitin<br>";
+    echo"    D                 G<br>";
+    echo"Baka sakaling ika’y magising<br>";
+    echo"    A               D   A<br>";
+    echo"Ang matigas mong damdamin<br>";
+
+
+    echo"[Chorus]<br>";
+
+    echo"D<br>";
+    echo"Di mo alam dahil sa`yo<br>";
+    echo"G<br>";
+    echo"Ako`y di makakain<br>";
+    echo"A<br>";
+    echo"Di rin makatulog <br>";
+    echo"D                    A<br>";
+    echo"Buhat ng iyong lokohin<br>";
+    echo"D<br>";
+    echo"Kung ako`y muling iibig <br>";
+    echo"G<br>";
+    echo"Sana`y di maging katulad mo<br>";
+    echo"A                        D  A<br>";
+    echo"Tulad mo nama`y pusong bato<br>";
+
+
+    echo"[Chorus]<br>";
+
+    echo"D#<br>";
+    echo"Di mo alam dahil sa`yo<br>";
+    echo"G#<br>";
+    echo"Ako`y di makakain<br>";
+    echo"A#<br>";
+    echo"Di rin makatulog <br>";
+    echo"D#                   A#<br>";
+    echo"Buhat ng iyong lokohin<br>";
+    echo"D#<br>";
+    echo"Kung ako`y muling iibig <br>";
+    echo"G#<br>";
+    echo"Sana`y di maging katulad mo<br>";
+    echo"A#                       D#  A#<br>";
+    echo"Tulad mo nama`y pusong bato<br>";
+
+    echo"D#<br>";
+    echo"Di mo alam dahil sa`yo<br>";
+    echo"G#<br>";
+    echo"Ako`y di makakain<br>";
+    echo"A#<br>";
+    echo"Di rin makatulog <br>";
+    echo"D#                   A#<br>";
+    echo"Buhat ng iyong lokohin<br>";
+    echo"D#<br>";
+    echo"Kung ako`y muling iibig <br>";
+    echo"G#<br>";
+    echo"Sana`y di maging katulad mo<br>";
+    echo"A#                        D#<br>";
+    echo"Tulad mo nama`y pusong bato<br>";
+    echo"A#                        D#<br>";
+    echo"Tulad mo nama`y pusong bato<br>";
+    echo"A#                        D#<br>";
+    echo"Tulad mo nama`y pusong bato<br>";
+    ?>
+</body>
